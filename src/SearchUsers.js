fb6a0c5a63b66803411ab27eaa95094735335fb5
Yohan0412 <112970445+Yohan0412@users.noreply.github.com>
2022-11-08 15:05:02 +0100

    Initial commit

---
+++ src/SearchUsers.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function () {
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  // TODO : fetch the github user list matching the search query
+  // (a new API call should be made every time the query changes)
+  // (errors should be logged in the console or/and printed to the UI)
+
+  useEffect(() => {
+    fetch(``)
+      .then((response) => response.json())
+      .then((data) => setResults());
+  }, []);
+
+  return (
+    <div>
+      <h2>Search for github users</h2>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {searchResults.map((githubUser) => (
+        <div key={githubUser.login}>{githubUser.login} </div>
+      ))}
+    </div>
+  );
+}
